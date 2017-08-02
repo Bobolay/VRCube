326402846b7a36ea4121eb767c8df6d970a1c32f
pkorenev <p.korenev@voroninstudio.eu>
2017-08-02 11:35:42 +0300

    add FileEditor assets to precompile; add config/locales/**/*.yml to I18n.load_path

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module VRCUbe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
+
+    # file_editor
+    config.assets.precompile += %w(fonts/octicons/octicons.woff cms/file_editor.css cms/file_editor.js)
   end
 end
